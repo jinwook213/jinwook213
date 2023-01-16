d6931bfcbf022ae6c36925d13a1772c48b48252e
jinwook213 <jinwook213@gmail.com>
2023-01-16 11:14:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='jinwook213',
+    name='MyGraph',
     version='0.0.1',
     description='jinwook pakage test',
     url='https://github.com/jinwook213/jinwook213.git',
     author = 'Jinwook_Lee',
     author_email= 'jinwook213@cau.ac.kr',
     license = 'jinwook',
-    packages = ['jinwook213'],
+    packages = ['MyGraph'],
     zip_safe= False,
     #install_requires = ['numpy==1.18.3']
     
